7ea866d0e7197605cc2f9c8e3752a37a25d460ad
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2018-06-18 21:06:30 -0300

    Adicionado novas telas
    Adicionado novas telas

---
+++ Teach.Grafico/TelaBaixadeFatura.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Teach.Grafico
+{
+    partial class TelaBaixadeFatura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.36F);
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tem certeza que deseja remover este cadastro?";
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(278, 92);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(75, 33);
+            this.btSalvar.TabIndex = 9;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(197, 92);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 33);
+            this.btCancelar.TabIndex = 8;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // TelaBaixadeFatura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 152);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btCancelar);
+            this.Name = "TelaBaixadeFatura";
+            this.Text = "Baixa de Fatura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Button btCancelar;
+    }
+}
